7ecd9a01457b7dd8655ed523648a3e86dfc81c78
David Viejo <dviejo@kungfusoftware.es>
2025-10-27 08:52:49 +0100

    feat: Integrate default crypto provider for TLS in Domains Plugin
    - Added installation of the default crypto provider from the `rustls` library in the `DomainsPlugin` implementation.
    - This enhancement improves the security and functionality of TLS certificate management within the plugin.

--- crates/temps-domains/src/plugin.rs
+++ crates/temps-domains/src/plugin.rs
@@ -13,6 +13,7 @@ use crate::{
     handlers::{self, create_domain_app_state, DomainAppState},
     tls::{repository::DefaultCertificateRepository, TlsServiceBuilder},
 };
+use rustls::crypto::CryptoProvider;
 
 /// Domains Plugin for managing DNS records and TLS certificates
 pub struct DomainsPlugin;
@@ -39,6 +40,7 @@ impl TempsPlugin for DomainsPlugin {
         context: &'a ServiceRegistrationContext,
     ) -> Pin<Box<dyn Future<Output = Result<(), PluginError>> + Send + 'a>> {
         Box::pin(async move {
+            CryptoProvider::install_default(rustls::crypto::ring::default_provider()).unwrap();
             // Get required dependencies from the service registry
             let db = context.require_service::<sea_orm::DatabaseConnection>();
             let encryption_service = context.require_service::<temps_core::EncryptionService>();
